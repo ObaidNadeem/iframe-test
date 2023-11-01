f30aae65a87852d2577adecdec7a0a537ef4e21d
Obaid Nadeem <obaidnadeem4@gmail.com>
2023-11-01 08:44:26 +0500

    iframe-objects

--- minting/script.js
+++ minting/script.js
@@ -1,20 +1,17 @@
-// Get the elements
-const decrementButton = document.getElementById('decrement');
-const incrementButton = document.getElementById('increment');
-const countElement = document.getElementById('count');
+// const decrementButton = document.getElementById('decrement');
+// const incrementButton = document.getElementById('increment');
+// const countElement = document.getElementById('count');
 
-// Initialize the counter value
-let count = parseInt(countElement.textContent);
+// let count = parseInt(countElement.textContent);
 
-// Add click event listeners
-decrementButton.addEventListener('click', () => {
-    if (count > 0) {
-        count--;
-        countElement.textContent = count;
-    }
-});
+// decrementButton.addEventListener('click', () => {
+//     if (count > 0) {
+//         count--;
+//         countElement.textContent = count;
+//     }
+// });
 
-incrementButton.addEventListener('click', () => {
-    count++;
-    countElement.textContent = count;
-}); 
+// incrementButton.addEventListener('click', () => {
+//     count++;
+//     countElement.textContent = count;
+// }); 
--- sneakPeek/script.js
+++
@@ -1,3 +0,0 @@
-new Splide('.splide', {
-    pagination: false,
-}).mount();
