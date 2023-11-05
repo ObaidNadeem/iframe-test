9d22818477aad8de06a72d1dc49931977071a1ad
Obaid Nadeem <obaidnadeem4@gmail.com>
2023-11-05 14:07:02 +0500

    css

--- lore/script.js
+++ lore/script.js
@@ -1,11 +1,3 @@
-const isMobileOrTablet = /Mobi|Tablet/i.test(navigator.userAgent);
-const loreScreen = document.querySelector('#loreScreen');
-
-if (isMobileOrTablet) {
-    loreScreen.classList.add('mobile-tablet-font');
-} else {
-    loreScreen.classList.remove('mobile-tablet-font');
-}
 new Splide('.splide', {
     type: 'loop',
     perPage: 3,
