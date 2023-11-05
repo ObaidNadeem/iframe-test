b2b894d4adc68f1df12824927a116b77782c5310
Obaid Nadeem <obaidnadeem4@gmail.com>
2023-11-05 14:26:18 +0500

    user agent test

--- lore/script.js
+++ lore/script.js
@@ -4,3 +4,12 @@ new Splide('.splide', {
     focus: 'none',
     pagination: false
 }).mount();
+
+const isMobileOrTablet = /Mobi|Tablet/i.test(navigator.userAgent);
+
+if (isMobileOrTablet) {
+    document.querySelector('#loreHeading h1').style.fontSize = '275%';
+    document.querySelector('#lore-card h1').style.fontSize = '150%';
+    document.querySelector('#lore-content p').style.fontSize = '175%';
+    document.querySelector('.loreCloseBtn h1').style.fontSize = '125%';
+}
