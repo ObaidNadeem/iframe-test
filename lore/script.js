8ea62658efd73b331c0b5a234c131b22cbcb4cb9
Obaid Nadeem <obaidnadeem4@gmail.com>
2023-11-05 14:45:40 +0500

    font-sizes-for-mobiles

--- lore/script.js
+++ lore/script.js
@@ -4,12 +4,3 @@ new Splide('.splide', {
     focus: 'none',
     pagination: false
 }).mount();
-
-const isMobileOrTablet = /Mobi|Tablet/i.test(navigator.userAgent);
-
-if (isMobileOrTablet) {
-    document.querySelector('#loreHeading h1').style.fontSize = '275%';
-    document.querySelector('#lore-card h1').style.fontSize = '150%';
-    document.querySelector('#lore-content p').style.fontSize = '175%';
-    document.querySelector('.loreCloseBtn h1').style.fontSize = '125%';
-}
